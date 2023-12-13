297fd264a53c491780bfc0934295e20889eac8a4
jhua-uml <vnendure@gmail.com>
2023-12-13 18:46:16 -0500

    updated

--- bot.py
+++ bot.py
@@ -81,7 +81,7 @@ def main():
     # stay invis in friends list
     if config["invisible"] == True:
         goInvisible()
-
+    
 
     # forceing no floor3 full clear with performance mode
     if config["performance"] == True:
@@ -1175,7 +1175,6 @@ def printResult():
 
 
 def useAbilities():
-    towerCheckCount = 0
     while True:
         diedCheck()
         healthCheck()
@@ -1370,25 +1369,13 @@ def useAbilities():
                 pydirectinput.press(config["awakening"])
                 # pydirectinput.press(config["meleeAttack"])
             elif states["status"] == "floor3" and checkFloor3Tower():
-                # Needs work maybe implement a checkFloor3Tower timeout then do              
-                print("tower check count: {}".format(towerCheckCount))
-                if not checkFloor2Elite() and not checkFloor2Mob() and towerCheckCount < 30:
+                if not checkFloor2Elite() and not checkFloor2Mob():
                     randomMove()
-                    checkFloor3Tower()  
-                    calculateMinimapRelative(states["moveToX"], states["moveToY"])
-                    moveToMinimapRelative(
-                        states["moveToX"], states["moveToY"], 1200, 1300, True
-                    )
-                elif not checkFloor2Elite() and not checkFloor2Mob() and towerCheckCount > 30:
                     checkFloor3Tower()
-                    calculateMinimapRelative(states["moveToX"], states["moveToY"])
-                    moveToMinimapRelative(
-                        -states["moveToX"], -states["moveToY"], 1200, 1300, True
-                    )
-                    randomMove()
-                    towerCheckCount = 0
-                    print("resetting position and tower check count")
-
+                calculateMinimapRelative(states["moveToX"], states["moveToY"])
+                moveToMinimapRelative(
+                    states["moveToX"], states["moveToY"], 1200, 1300, True
+                )
                 # if (
                 #     config["characters"][states["currentCharacter"]]["class"]
                 #     == "sorceress"
@@ -1396,7 +1383,6 @@ def useAbilities():
                 #     pyautogui.press("x")
                 sleep(200, 220)
                 clickTower()
-                towerCheckCount += 1
             elif states["status"] == "floor3" and checkFloor2Mob():
                 calculateMinimapRelative(states["moveToX"], states["moveToY"])
                 moveToMinimapRelative(
@@ -1781,6 +1767,7 @@ def checkFloor2Elite():
             )
             return True
     return False
+
 
 def checkFloor2Mob():
     minimap = pyautogui.screenshot(region=config["regions"]["minimap"])  # Top Right
@@ -2661,17 +2648,6 @@ def checkTimeout():
         states["timeoutCount"] = states["timeoutCount"] + 1
         return True
     return False
-
-# Adding a time limit on checking for floor3 tower and initiating random movev2 if so
-def checkTowerTimeout():
-    currentTime = int(time.time_ns() / 1000000)
-    if (
-        states["multiCharacterMode"] == True
-        and states["floor3Mode"] == True
-        and currentTime - states["instanceStartTime"] > config["towerTimeLimit"]
-    ):
-        print("character is stuck unable to find tower, initiating character reset")
-        return True
 
 
 def gameCrashCheck():
--- config.py
+++ config.py
@@ -25,7 +25,7 @@ from characters import characters
 
 config = {
     "mainCharacter": 0,  # must be in between number 0 to len(characters) - 1 (0 is the first character)
-    "GFN": True,  # set True for Geforce Now users
+    "GFN": False,  # set True for Geforce Now users
     "enableMultiCharacterMode": True,  # this is lit
     "enableLopang": False,  # NOTE: you need to setup bifrost locations properly for this, at very specific locations. Look up ^
     "enableGuildDonation": True,  # please make sure all your characters have a guild
